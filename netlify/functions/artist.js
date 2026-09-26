2eeef8302e34bcc9f17f125960637629f846fc7a
XiterBug <anakselatan34@gmail.com>
2026-09-26 16:42:29 +0700

    Delete netlify directory

--- netlify/functions/artist.js
+++
@@ -1,4 +0,0 @@
-const { wrap } = require('./_adapter');
-const handler = require('../../api/artist.js');
-
-exports.handler = wrap(handler);
--- netlify/functions/search.js
+++
@@ -1,4 +0,0 @@
-const { wrap } = require('./_adapter');
-const handler = require('../../api/search.js');
-
-exports.handler = wrap(handler);
